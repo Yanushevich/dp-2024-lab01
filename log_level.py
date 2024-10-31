2e577ef258d4726c18628bf2699cd057f9472848
Yanushevich <maksim_yanushevich@mail.ru>
2024-10-31 17:28:07 +0500

    feat(docstrings): Добавлена документация аргументов

--- log_level.py
+++ log_level.py
@@ -2,6 +2,16 @@ from enum import Enum
 
 
 class LogLevel(Enum):
+    """
+    Перечисление различных уровней логирования
+
+    Attributes:
+        INFO (str) - Информационные сообщения
+        TRACE (str) - Подробные сообщения для отслеживания выполнения
+        WARN (str) - Предупреждающие сообщения
+        ERROR (str) - Сообщения об ошибках, сбоях
+        FATAL (str) - Критические сообщения об ошибках
+    """
     INFO = "INFO"
     TRACE = "TRACE"
     WARN = "WARN"
